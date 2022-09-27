682ef683ccc987b95a38c234a02fa0ad03c13061
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-09-27 13:42:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const nombre = "Sergio";
+const apellido = "de Luque";
+const nombreCompleto = {
+    nombre, apellido
+}
+
+console.log(nombreCompleto);
+
+// sessionStorage.setItem("nombre-Completo", JSON.stringify(nombreCompleto));
+
+// localStorage.setItem("nombre-Completo", JSON.stringify(nombreCompleto));
+
+// document.cookie = "nombreCaducidad=nombre-Completo; expires=" + new Date(Date.now() + 120000).toUTCString()
+
+
